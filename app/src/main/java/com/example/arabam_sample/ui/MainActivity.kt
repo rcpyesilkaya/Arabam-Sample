afa0695e5fe9ff8fc54d24263e90aede4b77d38b
osahin45 <onur.sahin@arabam.com>
2021-04-23 17:53:50 +0300

    [feature] data and ui package created.

--- app/src/main/java/com/example/arabam_sample/ui/MainActivity.kt
+++ app/src/main/java/com/example/arabam_sample/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.arabam_sample.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.arabam_sample.R
 
 class MainActivity : AppCompatActivity() {
